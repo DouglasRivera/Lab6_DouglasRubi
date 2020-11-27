e0ce3f8f3c2a3734ccd18e66e8a810ec2b6feda9
DouglasRivera <riveradouglas19@gmail.com>
2020-11-27 15:30:41 -0600

    Clases creadas, se me reinicio y actualizo la compu

--- Lab6_DouglasRivera/src/lab6_douglasrivera/Lab6_DouglasRivera.java
+++ Lab6_DouglasRivera/src/lab6_douglasrivera/Lab6_DouglasRivera.java
@@ -15,7 +15,8 @@ public class Lab6_DouglasRivera {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+                new Menu().setVisible(true);
+
     }
     
 }
